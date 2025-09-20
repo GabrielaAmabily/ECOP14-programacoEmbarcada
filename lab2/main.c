7c3b928ebec48ff44b8b848da30d0b5e2953f73f
Gabriela Amabily <167456153+GabrielaAmabily@users.noreply.github.com>
2025-09-20 08:55:06 -0300

    Create main.c

---
+++ lab2/main.c
@@ -0,0 +1,15 @@
+#include "config.h"
+#include "pic18f4520.h"
+#include "lcd.h"
+#include "stdio.h"
+
+//inicio do programa
+
+void main(void) {
+    lcdInit();
+    //printf("oi gente testando isso");
+    printf("ECOP14\nIESTI UNIFEI");
+    
+    for (;;);
+  
+}
